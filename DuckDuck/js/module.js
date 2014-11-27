36677b9b3d1e1e233b51f5bc5ed9504ef0facfd6
h404 <hemanthkumar.k@outlook.com>
2014-11-27 23:12:40 +0530

    c1

--- DuckDuck/js/module.js
+++ DuckDuck/js/module.js
@@ -10,17 +10,5 @@
       app.controller('DetailsController', DetailsController);
       app.factory('SearchFactory', SearchFactory);
 
-      app.directive('ngEnter', function () {
-          return function (scope, element, attrs) {
-              element.bind("keydown keypress", function (event) {
-                  if (event.which === 13) {
-                      scope.$apply(function () {
-                          scope.$eval(attrs.ngEnter);
-                      });
-
-                      event.preventDefault();
-                  }
-              });
-          };
-      });
+     
   });
