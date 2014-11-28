47cd1d0ec34f5df063163f87c71348ea47d8e0f7
h404 <hemanthkumar.k@outlook.com>
2014-11-28 09:20:01 +0530

    c1

--- DuckDuck/js/DetailsController.js
+++ DuckDuck/js/DetailsController.js
@@ -6,9 +6,44 @@ define([], function () {
  
     function DetailsController($scope,$routeParams,SearchFactory) {
         
-        
-        var result = SearchFactory.getResult($routeParams.query).then(function (data) {
+        var massageData = function (data) {
             
+            var head = {
+
+            };
+
+            if(data.Image != "")
+            {
+                $scope.head.mainimg = data.Image;
+            }
+            else
+            {
+                //Need to handle no image scenario
+            }
+
+            if (data.Heading != "") {
+                $scope.head.title = data.Heading;
+            }
+            else {
+                //Need to handle
+            }
+
+            if (data.AbstractText != "") {
+                $scope.head.description = data.AbstractText;
+            }
+            else {
+
+            }
+
+
+
+            $scope.head = head;
+        };
+
+        var result = SearchFactory.getResult($routeParams.query).then(function (data) {
+            //$scope.$apply(function () {
+                massageData(data);
+            //});
         });
     }
 
--- DuckDuck/js/module.js
+++ DuckDuck/js/module.js
@@ -10,5 +10,45 @@
       app.controller('DetailsController', DetailsController);
       app.factory('SearchFactory', SearchFactory);
 
-     
+      app.directive('ngShowmore', function () {
+          return {
+              link: function ($scope, element, attrs) {
+                  
+                  var scope = $scope;
+                  var parent = element.parent();
+                  var paragrap = parent.find('p');
+                  var showmore = parent.find('.show_more');
+                  var hideit = parent.find('.hide_it');
+
+                  var show = function () {
+                      hideit.show();
+                      showmore.hide();
+                      parent.css({ 'overflow': 'visible' });
+                  };
+
+                  var hide = function () {
+                      hideit.hide();
+                      showmore.show();
+                      parent.css({ 'overflow': 'hidden' });
+                  };
+
+                  var topparent = parent.parent();
+
+                  $scope.$watch('head', function () {
+                      //if (paragrap.height() > parent.height()) {
+                          hide();
+                      //}
+
+                      showmore.click(show);
+                      hideit.click(hide);
+                  });
+
+                  $(paragrap).resize(function () {
+
+                     
+                  });
+
+              }
+          };
+      });
   });
